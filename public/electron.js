8db0454edfb5e0ca6df304769146ef3a7489fe88
Vicky Chijwani <vickychijwani@gmail.com>
2017-10-02 21:56:52 +0530

    Blank frameless launcher window with tray icon.

--- public/electron.js
+++ public/electron.js
@@ -1,14 +1,53 @@
-const {app, BrowserWindow} = require('electron');
+const {app, globalShortcut, BrowserWindow, Menu, Tray} = require('electron');
 
 const path = require('path');
 const isDev = require('electron-is-dev');
 
-let mainWindow;
+// globals to avoid being GC'ed
+let launcher, tray;
 
 function createWindow() {
-    mainWindow = new BrowserWindow({width: 900, height: 680});
-    mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
-    mainWindow.on('closed', () => mainWindow = null);
+    launcher = new BrowserWindow({
+        width: 750,
+        height: 450,
+        frame: false,
+        alwaysOnTop: true,
+        center: true,
+        closable: false,
+        minimizable: false,
+        maximizable: false,
+        moveable: false,
+        skipTaskbar: true,
+        show: false,
+    });
+    launcher.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
+    launcher.on('ready-to-show', () => launcher.show());
+    launcher.on('blur', () => launcher.hide());
+    launcher.on('closed', () => launcher = tray = null);
+
+    setupShortcuts();
+    setupTray();
+}
+
+function setupShortcuts () {
+    globalShortcut.register('Alt+Space', toggleLauncher);
+}
+
+function setupTray() {
+    const iconName = process.platform === 'win32' ? './icons/tray-32.ico' : './icons/tray-32.png';
+    const iconPath = path.join(__dirname, iconName);
+    tray = new Tray(iconPath);
+    tray.on('click', toggleLauncher);
+    tray.setToolTip('YouTube Launcher is running');
+    const contextMenu = Menu.buildFromTemplate([
+        { label: 'Toggle YouTube Launcher', click: toggleLauncher },
+        { label: 'Quit', click: () => app.quit() },
+    ]);
+    tray.setContextMenu(contextMenu);
+}
+
+function toggleLauncher() {
+    launcher.isVisible() ? launcher.hide() : launcher.show();
 }
 
 app.on('ready', createWindow);
@@ -20,7 +59,11 @@ app.on('window-all-closed', () => {
 });
 
 app.on('activate', () => {
-    if (mainWindow === null) {
+    if (launcher === null) {
         createWindow();
     }
+});
+
+app.on('will-quit', () => {
+    globalShortcut.unregisterAll();
 });
